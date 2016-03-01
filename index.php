6e89f86c36d9514613041c784b841a96a302b846
Omar Siam <Omar.Siam@oeaw.ac.at>
2016-03-01 12:59:26 +0100

    Example for a canned switch.php call.

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+$runner = true;
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/modules/fcs-aggregator/switch.php';
+
+use ACDH\FCSSRU\SRUWithFCSParameters,
+    ACDH\FCSSRU\switchAggregator\FCSSwitch;
+
+// Set up the parameter object
+// x-format=htmlpagetable&operation=searchRetrieve&version=1.2&
+// x-dataview=kwic,title&maximumRecords=10&x-context=pes_eng_032&
+// query=senses==Persian 
+$sru_fcs_params = new SRUWithFCSParameters("strict");
+$sru_fcs_params->xformat = 'htmlpagetable';
+$sru_fcs_params->operation = 'searchRetrieve';
+$sru_fcs_params->version = '1.2';
+$sru_fcs_params->xdataview = 'kwic,title';
+$sru_fcs_params->maximumRecords = '10';
+$sru_fcs_params->xcontext = 'pes_eng_032';
+$sru_fcs_params->context = array('pes_eng_032');
+$sru_fcs_params->query = 'senses==Persian';
+chdir(__DIR__ . '/modules/fcs-aggregator');
+$s = new FCSSwitch();
+$s->run();
+
+
